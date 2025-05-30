d8b54ad0fbdfdc78cab663adc6f72e057b3bcae5
urmishr <ramani.urmish@gmail.com>
2025-05-30 23:39:01 +0530

    fixed import bug

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./SIdebar";
+import Sidebar from "./Sidebar";
 import Header from "./Header";
 import styled from "styled-components";
 
